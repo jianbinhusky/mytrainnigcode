83d76438577e657037bf548d673baa8a1de21899
Joby JB4 Hu <hujb4@lenovo.com>
2016-03-14 16:01:43 +0800

    test for git flow

---
+++ SohuClient/src/main/java/cn/com/screendata/client/controller/lenovo/TestBi.java
@@ -0,0 +1,25 @@
+package cn.com.screendata.client.controller.lenovo;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by hujb4 on 2016/3/14.
+ */
+public class TestBi {
+    public static void main(String[] args) {
+        /**
+         *  qualitiy      price
+         *  2              8.88
+         *  3              9.32
+         *  4              14.44
+         *  ...
+         *  total   1321.31
+         */
+        Map<Double,Integer> map = new HashMap<Double, Integer>();
+        for (int i = 1; i <= 3; i++) {
+            
+        }
+
+    }
+}
